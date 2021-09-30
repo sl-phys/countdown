e26e2709ff1d24d57498bf73f404a4a2ec5b38b1
sl-phys <laverysam@hotmail.co.uk>
2021-10-01 00:01:49 +0100

    Slightly updated test_pep8, included hidden files

--- test_pep8.py
+++ test_pep8.py
@@ -2,12 +2,17 @@
 Test pep8
 A test file to see what the pep8 command (pep8 test_pep8.py) will pick up on.
 Created: 30.09.21;    Author: SL
-This is the "module docstring" which documents what the file contents are for and pylint gets sad if you don't have this.
+The "module docstring", documenting the file contents. pylint gets sad without.
 """
 
-VAR_1 = 123
+VAR_1 = 123  # Constants are named with UPPER_CASE naming style. -30.09.21
 VAR_2 = 555
 
 VAR_3 = VAR_1*VAR_2
+VAR_3 = VAR_3 % 5
+
+print('VAR_3 is now', VAR_3)
+
+VAR_3 = 5
 # Note: Command is now pycodestyle test_pep8.py - 30.09.21
-# Note: Also see pylint (pylint test_pep8.py) which gives a score for code.
+# Note: See pylint (pylint test_pep8.py) which scores code. -30.09.21
