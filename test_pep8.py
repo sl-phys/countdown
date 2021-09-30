bce39cc5ae068defa81e725ac8a7327adac1b686
sl-phys <laverysam@hotmail.co.uk>
2021-09-30 23:48:48 +0100

    Test file looks at pep8 and pylint

---
+++ test_pep8.py
@@ -0,0 +1,13 @@
+"""
+Test pep8
+A test file to see what the pep8 command (pep8 test_pep8.py) will pick up on.
+Created: 30.09.21;    Author: SL
+This is the "module docstring" which documents what the file contents are for and pylint gets sad if you don't have this.
+"""
+
+VAR_1 = 123
+VAR_2 = 555
+
+VAR_3 = VAR_1*VAR_2
+# Note: Command is now pycodestyle test_pep8.py - 30.09.21
+# Note: Also see pylint (pylint test_pep8.py) which gives a score for code.
